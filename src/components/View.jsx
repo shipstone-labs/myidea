7bb18a7fda899a87aea84e57d5ee7da8f9a0af91
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-08-03 15:40:11 -0400

    fix: Space on approve button

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -503,7 +503,7 @@ export const View = ({ row, onClose, requests }) => {
                                     <button
                                       type="button"
                                       disabled={requesting}
-                                      className={`inline-block gap-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 ${
+                                      className={`ms-3 inline-block gap-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 ${
                                         requesting
                                           ? "opacity-25"
                                           : "hover:bg-red-600 dark:hover:bg-red-300 dark:hover:text-black active:bg-red-400 dark:active:bg-red-500 active:shadow-none active:translate-x-[5px] active:translate-y-[5px]"
