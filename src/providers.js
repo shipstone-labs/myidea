4f45d82e5abb5637aef1e163d5342bef4b6eddd0
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-08-04 10:39:54 -0400

    fix: More work

--- src/providers.js
+++ src/providers.js
@@ -1,23 +1,19 @@
-import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { http } from "viem";
-import { lukso } from "wagmi/chains";
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import { lukso } from "viem/chains";
+
+const projectId = "205bd79347ff59d91896c6e2e36f8ee3";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "MyIdea",
+  description:
+    "MyIdea is a decentralized application for defining patent claims by the nanosecond.",
+};
 
 export const chains = [lukso];
+export const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
 
-export const config = defaultWagmiConfig({
-  chains,
-  multiInjectedProviderDiscovery: true,
-  transports: {
-    [lukso.id]: http(),
-  },
-  // projectId: feature.walletConnect.projectId,
-  metadata: {
-    name: "MyIdea",
-    description:
-      "MyIdea is a decentralized application for defining patent claims by the nanosecond",
-    url: "https://myidea.shipstone.com",
-    icons: ["https://myidea.shipstone.com/favicon-32x32.png"],
-  },
-  enableEmail: true,
-  ssr: true,
-});
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains });
